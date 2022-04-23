7eea78a20536fc5664c1443c3b4447da52c3f368
zer0 <internetghost0@protonmail.com>
2022-04-23 16:19:54 +0300

    now echoing for multiple clients simultaneously

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,20 @@ const ADDR: &str = "127.0.0.1:8080";
 async fn main() {
     // start to listening
     let listener = TcpListener::bind(ADDR).await.unwrap();
-    // new connection
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-    // create buffer to receiving bytes from user
-    let mut buffer = [0u8; 1024];
-    // receive bytes from user
-    let size = socket.read(&mut buffer).await.unwrap();
-    // send bytes to user
-    socket.write_all(&buffer[..size]).await.unwrap();
+    loop {
+        // new connection
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+        // create buffer to receiving bytes from user
+        let mut buffer = [0u8; 1024];
+        // to handle multiple clients simultaneously
+        tokio::spawn(async move {
+            loop {
+                // receive bytes from user
+                let size = socket.read(&mut buffer).await.unwrap();
+                // send bytes to user
+                socket.write_all(&buffer[..size]).await.unwrap();
+            }
+        });
+    }
 }
 
