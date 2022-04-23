3335db8805e631509567d5ad0142a5bf66796a4e
zer0 <internetghost0@protonmail.com>
2022-04-23 16:09:55 +0300

    echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+// Address for server
+const ADDR: &str = "127.0.0.1:8080";
+
+
+#[tokio::main]
+async fn main() {
+    // start to listening
+    let listener = TcpListener::bind(ADDR).await.unwrap();
+    // new connection
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+    // create buffer to receiving bytes from user
+    let mut buffer = [0u8; 1024];
+    // receive bytes from user
+    let size = socket.read(&mut buffer).await.unwrap();
+    // send bytes to user
+    socket.write_all(&buffer[..size]).await.unwrap();
 }
+
